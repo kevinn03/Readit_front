4572c829019fdd37886a4bb0b7ea12f7a1536d9d
Kevin <kevin03.nguyen@gmail.com>
2022-04-10 14:37:31 -0400

    Add time to right header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import './Header.css';
 const Header = () => {
+  const DAYS = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const date = new Date();
   return (
     <div className="dis-flex Header">
       <div className="header-left header-item">News Scraped From Reddit</div>
       <div className="header-title header-item">
         <span className="header-title__front">ReadIt</span> News
       </div>
-      <div className="header-right header-item">three</div>
+      <div className="header-right header-item">
+        <div>
+          {DAYS[date.getDay()]}, {MONTHS[date.getMonth()]} {date.getDate()}{' '}
+          {date.getFullYear()}
+        </div>
+        <div>
+          {date.getHours()}:{date.getMinutes()}:
+          {date.getMilliseconds().toString().slice(0, 2)}
+        </div>
+      </div>
     </div>
   );
 };
