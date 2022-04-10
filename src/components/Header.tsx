22e7bb546a0b582a63333316def3fa78a6f0d497
Kevin <kevin03.nguyen@gmail.com>
2022-04-10 13:57:53 -0400

    test style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,11 @@ import './Header.css';
 const Header = () => {
   return (
     <div className="dis-flex Header">
-      <div className="">one</div>
-      <div className="">two</div>
-      <div className="">three</div>
+      <div className="header-left header-item">News Scraped From Reddit</div>
+      <div className="header-title header-item">
+        <span className="header-title__front">ReadIt</span> News
+      </div>
+      <div className="header-right header-item">three</div>
     </div>
   );
 };
