999ebbb5033e5810afa92c226fc4704ff905f2c8
Kevin <kevin03.nguyen@gmail.com>
2022-04-11 14:21:46 -0400

    fix navbar

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import './SubHeader.css';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown, Form } from 'react-bootstrap';
 const SubHeader = () => {
   return (
     <div className="Subheader">
-      <Navbar bg="light" variant="light">
+      <Navbar className="subheader-nav" bg="light" expand="md">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <Nav.Link href="#link">Yo</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </div>
