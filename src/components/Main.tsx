a9f944f47c4af6bf7ca0ad4cf4a1779f4d94c410
Kevin <kevin03.nguyen@gmail.com>
2022-04-11 20:50:43 -0400

    Add sorted sports posts

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Main.css';
 import Post from './Post';
+import redditAPI from '../services/redditAPI';
 import { PostInter } from '../types';
-// import '../App.css';
-interface IProps {
-  posts: PostInter[];
-}
-const Main = ({ posts }: IProps) => {
+
+const Main = () => {
+  const [sportPosts, setSportPosts] = useState<PostInter[]>([]);
+
+  useEffect(() => {
+    const addSportPosts = async () => {
+      const subreddit = [
+        { r: 'nba', index: 2 },
+        { r: 'nfl', index: 2 },
+        { r: 'nhl', index: 2 },
+        { r: 'baseball', index: 2 },
+      ];
+      const tempPost: PostInter[] = [];
+      for (const sub of subreddit) {
+        const result = await redditAPI.getPosts(sub.r, sub.index);
+        tempPost.push(...result);
+      }
+      console.log(tempPost.sort((a, z) => (a.score < z.score ? 1 : -1)));
+      setSportPosts(tempPost);
+    };
+
+    addSportPosts();
+  }, []);
   return (
     <div className="Main h-100 border-box">
-      <Post title={'Global'} posts={posts}></Post>
-      <Post title={'Test'} posts={posts}></Post>
+      {/* <Post title={'World News'} posts={posts}></Post> */}
+      {/* <Post title={'CDN News'} posts={posts}></Post> */}
+      {/* <Post title={'Entertainment'} posts={posts}></Post> */}
+      <Post title={'Sports'} posts={sportPosts}></Post>
+      {/* <Post title={'Politics'} posts={posts}></Post> */}
+      {/* <Post title={'Business'} posts={posts}></Post> */}
+      {/* <Post title={'Technology'} posts={posts}></Post> */}
     </div>
   );
 };
--- src/services/redditAPI.ts
+++ src/services/redditAPI.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 const baseURL = '/api/reddit';
 
-const getPosts = async (subreddit: string) => {
-  const response = await axios.get(`${baseURL}/${subreddit}`);
+const getPosts = async (subreddit: string, index = 0) => {
+  const response = await axios.get(
+    `${baseURL}/${subreddit}?index=${index.toString()}`
+  );
   return response.data;
 };
 
